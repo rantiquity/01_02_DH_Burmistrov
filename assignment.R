b0cbb20113223eb703e5a22573692a53b0900c56
NikolayB02 <nikolay.burmistrov.02@mail.ru>
2026-09-19 18:36:21 +0300

    Implement data filtering and transformation steps

--- assignment.R
+++ assignment.R
@@ -8,17 +8,25 @@ works <- gutenberg_works()
 
 # (1) Отберите ряды, в которых gutenberg_author_id равен 65 или 410;
 # после этого выберите два столбца: author, title
-my_data <- works |> 
-  # ваш код здесь
 
+  # ваш код здесь
+my_data <- works|>
+  filter(gutenberg_author_id == 65 | gutenberg_author_id == 410) |>
+    select(author, title)
 # (2) Используйте функцию separate(), чтобы разделить 
 # столбец с именем и фамилией на два новых: author, name. 
 # Удалите столбец name
-my_data2 <- my_data |>
-  # ваш код здесь
 
+  # ваш код здесь
+my_data2 <- my_data |>
+  separate(author, sep = ", ", into = c("author", "name")) |>
+  select(-name)
 # (3) Используйте group_by() и summarise(), чтобы узнать,
 # сколько произведений Шекспира и Марлоу хранится в библиотеке Gutenberg
 # новый столбец должен называться n, не делайте сортировку 
-my_data3 <- my_data2 |>
+
   # ваш код здесь
+my_data3 <- my_data2 |>
+  group_by(author) |>
+  summarise(n = n())
+
